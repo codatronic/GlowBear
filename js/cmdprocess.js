9368ce60339f67b27f8d54efd802906f5a6c7fcc
codatronic <codatronic@users.noreply.github.com>
2014-03-19 20:19:30 -0400

    Update cmdprocess.js

--- js/cmdprocess.js
+++ js/cmdprocess.js
@@ -1,6 +1,9 @@
      var e1entry = 1
      var area1 = 'GlowBear Indevelopment Edition <br> A simple JavaScript based web terminal that supports commands and stuff <br> Use help command for command listing'
      var dowrite = 'hi'
+     var c1 = 'hi'
+     var ca = 'hi'
+     var l = 1
      document.getElementById('echo1').innerHTML = area1;
           
      function setcp(p) { // set the currently running process
@@ -25,6 +28,19 @@
              	if (e1entry == "repo") {
              		window.location.assign('https://github.com/codatronic/GlowBear')
              	}else{
+             		if (e1entry.substring(0,9) == "compound ") {
+             			// do stuff
+             			c1 = e1entry.substring(9);
+             			ca = c1.split(',');
+             			l = ca.length;
+             			for(i = 0; i < l; i++){
+             				// other stuff
+             				document.getElementById('echo1form').innerHTML = ca[i];
+             				validecho1()
+             				
+             			}
+             			
+             		}else{
              setcp('notvalid')
 	     area1 = area1 + ' <br/> ' + e1entry + ' is not a valid GlowBear command';
              }
